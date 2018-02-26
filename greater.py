24073bf0e851341ca68b8c15d955bb3d53154fe6
leeladevi31 <36396489+leeladevi31@users.noreply.github.com>
2018-02-26 03:48:58 -0800

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,9 @@
+a = float(input("enter the first number"))
+b = float(input("enter the second number"))
+c = float(input("enter the third number"))
+if (a>b) and (a>c):
+    print(a)
+if(b>a) and (b>c):
+    print(b)
+if(c>a) and (c>b):
+    print(c)
