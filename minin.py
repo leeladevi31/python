44c8d97a424061782b512b633e9013066aeff242
leeladevi31 <36396489+leeladevi31@users.noreply.github.com>
2018-03-04 08:48:48 -0800

    Create minin.py

---
+++ minin.py
@@ -0,0 +1,8 @@
+lst = []
+num = int(input('How many numbers: '))
+
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+
+print("Minimum element in the list is :", min(lst))
